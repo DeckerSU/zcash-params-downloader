6776337a99bd1b8ec5eb655e4d9cbaf96e151733
DeckerSU <deckersu@protonmail.com>
2023-09-05 19:04:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+### zcash params downloader
+
+The `zcash params downloader` is a utility, similar to the script `zcutil/fetch-params.sh` used in the Zcash software, that is designed to download the Zcash Params files. These files are essential for the correct operation of the Zcash network and for maintaining transaction privacy and security.
+
+The purpose of this utility is to retrieve the most up-to-date version of the Params files from a trusted source.
+
+It performs the following steps:
+
+1. The utility checks for the existence of the following files in the current directory:
+
+- `sprout-proving.key`
+- `sprout-verifying.key`
+- `sapling-spend.params`
+- `sapling-output.params`
+- `sprout-groth16.params`
+
+2. If any of the files from the list (1) are not found, the utility will download them from a trusted source (z.cash) in parallel mode. After the download is finished, it will verify the sha256 checksum of the downloaded file. If a file has already been downloaded and exists, it will only verify its hash without downloading it again. If the hash is incorrect, indicating an incomplete download or other issues, the user should delete the file and restart the utility.
+
+#### Useful links (dev)
+
+-  https://github.com/search?q=resp.into_body%28%29+download+language%3ARust&type=code&p=2
+-  https://github.com/agnes-rs/agnes/blob/22f94409fc20da218a35b9161a9f170280c7481f/src/source/file.rs#L101
+-  https://github.com/Byron/git-tool/blob/5f3da85b4f682a12a766dec3c57fc6a3f9f76391/src/update/github.rs#L146
+-  https://github.com/k8s-rs/kubernetes-rs/blob/dd29adbab79c58270388010462cc3b4c4f79ac7b/apimachinery/src/client.rs#L112
+-  https://github.com/AOSC-Dev/ciel-rs/blob/861b8afe9fd0b972a5331e47f476dbfe676040fb/src/network.rs#L58
+
+-  https://gist.github.com/benkay86/6afffd4cf90ad84ac43e42d59d197e08
+-  https://github.com/rgreinho/trauma/blob/0950ca3c54f625fc9b77a35cedd5967deb5c9f42/src/downloader.rs#L92 (!) <- chunks
+-  https://github.com/hhatto/rust-snippets/blob/8f188d172ee0e3fcceb21b037d7c4d20919ab1d6/http-client/src/http2-hyper.rs#L87
+
+
---
+++ src/main.rs
@@ -0,0 +1,265 @@
+/*
+    rustup target add x86_64-pc-windows-gnu
+    sudo apt-get install mingw-w64
+    cargo build --target x86_64-pc-windows-gnu
+*/
+
+use std::io;
+use std::sync::Arc;
+
+use hyper::client::HttpConnector;
+use hyper_rustls::HttpsConnector;
+use hyper::{Client, Uri, Response, StatusCode, Request, Method, Body};
+use indicatif::{ProgressBar, ProgressStyle, MultiProgress};
+use sha2::digest::generic_array::GenericArray;
+use tokio::fs::{File, self};
+use hyper::body::HttpBody;
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use sha2::{Sha256, Digest};
+use thiserror::Error;
+use hex::FromHex;
+
+#[derive(Debug, Error)]
+pub enum DownloadError {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+
+    #[error("failed to open file")]
+    FileOpen,
+
+    #[error("url parsing failed")]
+    UrlParse,
+
+    #[error("redirect location conversion error")]
+    LocationError,
+
+    #[error("Http response error: {0}")]
+    HyperError(#[from] hyper::Error),
+}
+
+#[derive(Debug, Error)]
+pub enum ChecksumError {
+    #[error("checksum failed; expected {}, found {}", expected, found)]
+    Checksum { expected: String, found: String },
+
+    #[error("expected checksum isn't a valid checksum")]
+    InvalidInput,
+
+    #[error("File read error: {0}")]
+    ReadFile(#[from] std::io::Error),
+
+    #[error("Invalid hex conversion: {0}")]
+    HexFromError(#[from] hex::FromHexError),
+}
+
+pub async fn file_exists(file_path: &str) -> bool {
+    let metadata = fs::metadata(file_path).await;
+
+    match metadata {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
+
+#[tokio::main]
+async fn main() {
+
+    let filenames = [
+        "sprout-proving.key.deprecated-sworn-elves",
+        "sprout-verifying.key",
+        "sapling-spend.params",
+        "sapling-output.params",
+        "sprout-groth16.params"
+    ];
+
+    let hashes = [
+        "8bc20a7f013b2b58970cddd2e7ea028975c88ae7ceb9259a5344a16bc2c0eef7",
+        "4bd498dae0aacfd8e98dc306338d017d9c08dd0918ead18172bd0aec2fc5df82",
+        "8e48ffd23abb3a5fd9c5589204f32d9c31285a04b78096ba40a79b75677efc13",
+        "2f0ebbcbb9bb0bcffe95a397e7eba89c29eb4dde6191c339db88570e3f3fb0e4",
+        "b685d700c60328498fbde589c8c7c484c722b788b265b72af448a5bf0ee55b50"
+    ];
+
+
+    let https = hyper_rustls::HttpsConnectorBuilder::new()
+        .with_native_roots()
+        .https_or_http()
+        .enable_http1()
+        .enable_http2()
+        .build();
+
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
+    let base_url = "https://z.cash/downloads";
+    let multi = Arc::new(MultiProgress::new());
+
+    let mut tasks = Vec::new();
+
+    for filename in filenames {
+        let url = format!("{}{}", base_url, filename);
+        let local_filename = match filename {
+            "sprout-proving.key.deprecated-sworn-elves" => "sprout-proving.key",
+            _ => filename
+        };
+        
+        if !file_exists(local_filename).await {
+            let client = client.clone();
+            let multi = multi.clone();
+
+            // Create new Task for each filename and add to the tasks Vec
+            let task = tokio::spawn(async move {
+                if let Err(e) = download_file(&client, url, local_filename.to_string(), multi).await {
+                    println!("Error downloading {}: {:?}", local_filename, e);
+                }
+            });
+
+            tasks.push(task);
+        }
+    }
+
+    for task in tasks {
+        if let Err(e) = task.await {
+            println!("Task join error: {:?}", e);
+        }
+    }
+
+    
+    for (filename, hash) in filenames.iter().zip(hashes.iter()) {
+
+        let local_filename = match *filename {
+            "sprout-proving.key.deprecated-sworn-elves" => "sprout-proving.key",
+            _ => filename
+        };
+
+        if file_exists(local_filename).await {
+            print!("Filename: {}", local_filename);
+            let mut file = match File::open(&local_filename).await {
+                Ok(file) => file,
+                Err(error) => {
+                    println!("An error occurred while opening the file: {}", error);
+                    continue; // Skip the rest of the loop
+                }
+            };
+
+            match validate_checksum(&mut file, hash).await {
+                Ok(_) => {
+                    println!(" - Ok!")
+                },
+                Err(e) => {
+                    println!(" - Error: {}", e.to_string());
+                }
+            };
+            
+        }
+    }
+}
+
+async fn download_file(client: &Client<HttpsConnector<HttpConnector>>, url: String, filename:String, multi: Arc<MultiProgress>) -> Result<(), DownloadError> {
+    // Try to open the file.
+    let mut file = File::create(&filename).await?;
+
+    let mut uri = url.parse::<Uri>().map_err(|_err| DownloadError::UrlParse)?;
+    
+    let mut response: Response<_>;
+
+    loop {
+        // Try to fetch the URL.
+        let req = Request::builder()
+            .method(Method::GET)
+            .uri(uri)
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+            .body(Body::from(""))
+            .unwrap();
+
+        response = client.request(req).await?;
+        // response = client.get(uri.to_owned()).await?;
+
+        // Check if the response is a redirect.
+        if response.status().is_redirection() {
+            // Get the redirect location.
+            let location = response.headers().get("location").ok_or(DownloadError::LocationError)?;
+            let location_str = location.to_str().map_err(|_err| DownloadError::LocationError)?;
+            uri = location_str.parse::<hyper::Uri>().map_err(|_err| DownloadError::LocationError)?;
+        } else {
+            break;
+        }
+    }
+
+    // Process the stream.
+    // let stream = response.into_body();
+
+    // We should not use this here because the files are too big. Instead, we should receive the response in chunks and write these chunks to a file while displaying the progress.
+    // let data = hyper::body::to_bytes(stream).await?; 
+
+    assert_eq!(response.status(), StatusCode::OK);
+    // assert!(response.headers().get(hyper::http::header::CONTENT_DISPOSITION).is_none());
+
+    let content_length = response.headers()
+        .get(hyper::header::CONTENT_LENGTH)
+        .and_then(|value| value.to_str().ok())
+        .and_then(|value| value.parse().ok())
+        .unwrap_or(0);
+
+    let progress_bar = multi.add(ProgressBar::new(content_length));
+    // progress_bar.set_length(content_length);
+    let template_begin_str = r#"{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})"#;
+    let template = format!("{} | {}", template_begin_str, filename);
+
+    // "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})"
+    progress_bar.set_style(ProgressStyle::default_bar()
+        .template(&template).unwrap());
+    // make sure we show up at all.  otherwise no rendering // event.
+    progress_bar.tick();
+
+    let mut body = response.into_body();
+    
+    let mut bytes_downloaded = 0_usize;
+
+    while let Some(buf) = body.data().await {
+        let buf = buf?;
+        bytes_downloaded = bytes_downloaded + buf.len();
+        progress_bar.set_position(bytes_downloaded as u64);
+        file.write_all(&buf).await?;
+    }
+    
+    // progress_bar.finish();
+    // progress_bar.finish_with_message("download complete");
+    progress_bar.finish_and_clear();
+
+    Ok(())
+
+}
+
+pub async fn validate_checksum(file: &mut File, checksum: &str) -> Result<(), ChecksumError> {
+
+    let expected = <[u8; 32]>::from_hex(checksum)
+        .map(GenericArray::from)
+        .map_err(|_| ChecksumError::InvalidInput)?;
+
+    let mut hasher = Sha256::new();
+    let mut buffer = vec![0u8; 8 * 1024];
+
+    loop {
+        match file.read(&mut buffer).await? {
+            0 => break,
+            read => hasher.update(&buffer[..read]),
+        }
+    }
+
+    let found = hasher.finalize();
+
+    // let mut hash_string = String::new();
+    // for byte in found.iter() {
+    //     hash_string.push_str(&format!("{:02x}", byte));
+    // }
+    // println!("{}", hash_string);
+
+    if *found != *expected {
+        return Err(ChecksumError::Checksum {
+            expected: checksum.into(),
+            found:    format!("{:x}", found),
+        });
+    }
+
+    Ok(())
+}
